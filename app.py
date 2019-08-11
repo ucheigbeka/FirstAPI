63e15bc8fdba9d1c19475c5199cf55f5a4261b90
Uchechi Akah <akah.uchechi@gmail.com>
2019-08-11 06:26:51 +0100

    add working api to POST products and GET all products

--- app.py
+++ app.py
@@ -5,11 +5,55 @@ import os
 
 #Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'msg':'Ataman is here!'})
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Init Database
+# global db 
+db = SQLAlchemy(app)
+
+# Init marshmallow
+ma = Marshmallow(app)
+
+# Asset class
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True)
+
+    def __init__(self, name):
+        self.name = name
+
+# Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name')
+
+# init schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
+
+# Create  product
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+
+    new_product = Product(name)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+# Get all products
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result.data)
+    
 #Run server
 if __name__ == "__main__":
     app.run(debug=True)
